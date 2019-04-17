f76bb28c394e75ebddf577b04e0397af859bcdad
pointblank1123 <aeputman02@gmail.com>
2019-04-17 11:53:41 -0600

    Intellij Initial Commit

---
+++ MultiUserChat/ChatServer/src/com/muc/ServerMain.java
@@ -0,0 +1,28 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created 04-15-19
+*/
+public class ServerMain {
+    public static void main(String[] args){
+        int port = 8818;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(true){
+                System.out.println("About to accept client connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("Hello World\n Alex P.\n".getBytes());
+                clientSocket.close();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
