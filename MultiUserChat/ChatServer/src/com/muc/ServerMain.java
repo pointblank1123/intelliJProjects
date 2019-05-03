67786630cf889ca3b02bfdf1e6c7df2067480c45
pointblank1123 <aeputman02@gmail.com>
2019-05-03 12:50:40 -0600

    Update 05-3-19.1 Login & user online notification

--- MultiUserChat/ChatServer/src/com/muc/ServerMain.java
+++ MultiUserChat/ChatServer/src/com/muc/ServerMain.java
@@ -1,27 +1,14 @@
 package com.muc;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 /**
  * Created 04-15-19
 */
 public class ServerMain {
     public static void main(String[] args){
         int port = 8818;
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            while(true){
-                System.out.println("About to accept client connection...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from " + clientSocket);
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
-            }
-        }catch(IOException e) {
-            e.printStackTrace();
-        }
+        Server server = new Server(port);
+
+        server.start();
     }
 
 
--- MultiUserChat/ChatServer/src/com/muc/ServerWorker.java
+++ MultiUserChat/ChatServer/src/com/muc/ServerWorker.java
@@ -1,13 +1,21 @@
 package com.muc;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
+
 
 public class ServerWorker extends Thread {
     private final Socket clientSocket;
+    private final Server server;
+    private String login = null;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket ClientSocket) {
+    public ServerWorker(Server server, Socket ClientSocket) {
         this.clientSocket = ClientSocket;
+        this.server = server;
     }
     @Override
     public void run(){
@@ -20,19 +28,69 @@ public class ServerWorker extends Thread {
         }
     }
 
-    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+    private void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
         InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
+        this.outputStream = clientSocket.getOutputStream();
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while((line=reader.readLine())!= null){
-            if("quit".equalsIgnoreCase(line)){
-                break;
+            String[] tokens = StringUtils.split(line);
+            /*  String Utils, Apache Class
+                Download from commons.apache.org/proper/commons-lang/download_lang.cgi
+                Extract: intelliJ File/Project Structure click + and find commons-lang#.#.#.jar
+                ----------------Method 2---------------
+                Type String[] tokens = Line.split("");
+             */
+            if (tokens != null && tokens.length > 0) {
+                String cmd = tokens[0];
+                if ("quit".equalsIgnoreCase(cmd)) {
+                    break;
+                } else if("login".equalsIgnoreCase(cmd)){
+                    handleLogin(outputStream, tokens);
+                } else {
+                    String msq = "Unknown Command " + cmd + "\n";
+                    outputStream.write(msq.getBytes());
+                }
+                /*
+                String msg = "You Typed: " + line + "\n";
+                outputStream.write(msg.getBytes());
+                 */
             }
-            String msg = "You typed: " + line + "\n";
-            outputStream.write(msg.getBytes());
         }
         //outputStream.write("Hello World\n Alex P.\n".getBytes());
         clientSocket.close();
+    }
+
+    private String getLogin(){
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        if(tokens.length == 3){
+            String login =tokens[1];
+            String password = tokens[2];
+
+            if((login.equals("guest") && password.equals("guest")) || (login.equals("jim") && password.equals("jim"))){
+                String msg = "Ok Login\n ";
+                outputStream.write(msg.getBytes());
+                this.login = login;
+                System.out.println("User Logged In Successfully :" + login);
+
+                String onlineMsg = "Online " + login +"\n";
+                List<ServerWorker> workerList = server.getWorkerList();
+                for(ServerWorker worker: workerList) {
+                    worker.send(onlineMsg);
+                }
+            } else{
+                String msg = "Error Login\n";
+                outputStream.write(msg.getBytes());
+            }
+        }
+    }
+
+    private void send(String onlineMsg) throws IOException {
+        outputStream.write(onlineMsg.getBytes());
+
     }
 }
